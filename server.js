e7f6bad3a5d3f04812bd7fee999e075806156dfd
Shivasai-k <shivasai.karampuri@gmail.com>
2024-11-22 19:00:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const cors=require('cors')
+const morgan=require('morgan')
+const dotenv=require('dotenv')
+const colors=require('colors')
+const connectDb = require('./config/connectDB')
+
+//config dot env file
+dotenv.config()
+
+//database call
+connectDb();
+
+//rest object
+const app=express()
+
+//middleware
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(cors())
+
+//routes
+//user routes
+app.use("/api/v1/users",require('./routes/userRoute'))
+
+//transactions routes
+app.use('/api/v1/transactions',require('./routes/transactionRoutes'))
+
+//port
+const PORT=8080 || process.env.PORT
+
+//listen server
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+})
